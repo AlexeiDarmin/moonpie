fb103f2132610710054bb80587e89fba65cf1bfe
Alexei Darmin <alexei.darmin@gmail.com>
2021-10-25 16:56:22 -0400

    fix failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders All your moonpie are belong to us. link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/All your moonpie are belong to us./i);
   expect(linkElement).toBeInTheDocument();
 });
